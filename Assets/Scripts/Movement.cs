4c4d9c1b2dfee3967b19a2810bbfd44af37c3961
JPotterf <Jacob.Potterf@gmail.com>
2020-10-25 20:13:33 +0200

    added assets and setup basic player movement and camera

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public float rotateSpeedMovement = 0.1f;
+    float rotateVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            //if raycast is hitting something with the navmesh activated
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                //player moves to the raycast point
+                agent.SetDestination(hit.point);
+
+                //rotation
+                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+                    rotationToLookAt.eulerAngles.y,
+                    ref rotateVelocity,
+                    rotateSpeedMovement * (Time.deltaTime * 5));
+
+                transform.eulerAngles = new Vector3(0, rotationY, 0);
+            }
+        }
+    }
+}
